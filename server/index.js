9f059f6ca7cb5264466dc8477ff269a0af1a7c9d
raghvendrasingh0871 <raghvendrasingh0871@gmail.com>
2025-04-01 18:53:44 +0530

    added server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 dotenv.config();
-
+connectDB();
 const app = express();
 const PORT=process.env.PORT;
 
